007647938962f22d02d5bedf8a2d9448849b3ac8
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-09-03 22:46:26 +0100

    Adding tests for the Car class 'attributes', '.colour' class method and '#full_name' instance method.

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,22 +2,54 @@ require 'car'
 
 describe 'Car' do
 
-  it "allows reading for :wheels" do 
+  describe 'attributes' do
+
+    it "allows reading and writing for :make" do
+      car = Car.new
+      car.make = 'Test'
+      expect(car.make).to eq('Test')
+    end
+
+    it "allows reading and writing for :year" do
+      car = Car.new
+      car.year = 9999
+      expect(car.year).to eq(9999)
+    end
+
+    it "allows reading and writing for :colour" do
+      car = Car.new
+      car.colour = 'foo'
+      expect(car.colour).to eq('foo')
+    end
+
+    it "allows reading for :wheels" do
+      car = Car.new
+      expect(car.wheels).to eq(4)
+    end
 
   end
 
   describe '.colours' do
+
     it "returns an array of colour names" do
       c = ['blue', 'black', 'red', 'green']
       expect(Car.colours).to match_array(c)
     end
+
   end
 
-  # or
-  # context '.colours' do
-  #   it "returns an array of colour names" do
+  describe '#full_name' do
+    it "returns a string in the expected format" do
+      @honda = Car.new(make: 'Honda', year: 2004, colour: 'blue')
+      expect(@honda.full_name).to eq('2004 Honda (blue)')
+    end
 
-  #   end
-  # end
+    context 'when initialized with no arguments' do
+      it 'returns a string using default values' do
+        car = Car.new
+        expect(car.full_name).to eq ('2007 Volvo (unknown)')
+      end
+    end
+  end
 
 end
