52640216b22eab1a6365cd1305b5d147e7a37978
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-09-05 10:31:47 +0100

    Practising testing using 'Compound' expectations and 'Composing' matchers.

--- spec/expectations_matchers_spec.rb
+++ spec/expectations_matchers_spec.rb
@@ -283,7 +283,60 @@ describe 'Expectation Matchers' do
 
       expect { warn('problem') }.to output(/problem/).to_stderr
     end
-
   end
 
+  describe 'compound expectations' do
+    it 'will match using: and, or, &, |' do
+      expect([1,2,3,4]).to start_with(1).and end_with(4)
+      expect([1,2,3,4]).to start_with(1) & include(2)
+      expect(10 * 10).to be_odd.or be > 50
+
+      array = ['hello', 'goodbye'].shuffle
+      expect(array.first).to eq("hello") | eq("goodbye")
+    end
+  end
+
+  describe 'composing matchers' do
+    # Some matchers accept matchers as arguments. (new in rspec3)
+
+    it 'will match all collection elements using a matcher' do
+      array = [1,2,3]
+      expect(array).to all(be < 5)
+    end
+
+    it 'will match by sending matchers as arguments to matchers' do
+      string = "hello"
+      expect {string = "goodbye"}.to change {string}.from( match(/ll/) ).to( match(/oo/) )
+
+      hash = {a: 1, b: 2, c: 3}
+      expect(hash).to include(a: be_odd, b: be_even, c: be_odd)
+      expect(hash).to include(a: be > 0, b: be_within(2).of(4))
+    end
+
+    it 'will match using noun-phrase aliases for matchers' do
+      # These are built-in aliases that make
+      # specs read better by using noun-based
+      # phrases instead of verb-based phrases.
+
+      # valid but awkward example:
+      fruits = ['apples', 'banana', 'cherry']
+      expect(fruits).to start_with( start_with('a') ) & include( match(/a.a.a/) ) & end_with( end_with('y') )
+
+      # Improved version of the previous example 
+      # 'start_with' becomes 'a_string_starting_with'
+      # 'end_with' becomes 'a_string_ending_with'
+      # 'match' becomes 'a_string_matching'
+
+      fruits = ['apples', 'banana', 'cherry']
+      expect(fruits).to start_with( a_string_starting_with('a')) & include(a_string_matching(/a.a.a/)) & end_with(a_string_ending_with('y'))
+
+      # Valid but awkward example:
+      array = [1,2,3,4]
+      expect(array).to start_with( be <= 2 ) | end_with( be_within(1).of(5) )
+
+      # Improved version of the previous example
+      array = [1,2,3,4]
+      expect(array).to start_with( a_value <= 2 ) | end_with( a_value_within(1).of(5) )
+    end
+  end
 end
