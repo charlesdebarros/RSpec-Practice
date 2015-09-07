02c2ba8211c69ec3b0517bde408153cb8164a5cf
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-09-07 19:51:43 +0100

    Extracting shared examples to be used by different classes' tests, in this case 'Car' and 'Truck'.

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,45 +1,9 @@
 require 'car'
+require 'shared_examples/a_standard_vehicle'
 
 describe Car do
 
-  describe 'attributes' do
-
-    # Do not need subject explicitly-defined
-    # if we use a classname after 'describe'
-    # subject { Car.new }
-
-    # use 'subject' instead of 'let'
-    # if variable is the subject of the example
-    # let(:car) { Car.new }
-
-    # 'let' is better than 'before' for
-    # setting up instance variables
-    # before(:example) do 
-    #   @car = Car.new
-    # end
-
-    it "allows reading and writing for :make" do
-      subject.make = 'Test'
-      expect(subject.make).to eq('Test')
-    end
-
-    it "allows reading and writing for :year" do
-      subject.year = 9999
-      expect(subject.year).to eq(9999)
-    end
-
-    it "allows reading and writing for :colour" do
-      subject.colour = 'foo'
-      expect(subject.colour).to eq('foo')
-    end
-
-    it "allows reading for :wheels" do
-      expect(subject.wheels).to eq(4)
-    end
-
-    it "allows writing for :doors" 
-
-  end
+  it_behaves_like('a standard vehicle')
 
   describe '.colours' do
 
@@ -48,7 +12,6 @@ describe Car do
     it "returns an array of colour names" do
       expect(Car.colours).to match_array(colours)
     end
-
   end
 
   describe '#full_name' do
