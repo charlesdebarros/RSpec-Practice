de4a2a95f32fd3f2f07c35ea918ae253fe3202b6
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-09-07 19:03:31 +0100

    Replacing the 'before' hook with a 'let' method.

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -4,27 +4,31 @@ describe 'Car' do
 
   describe 'attributes' do
 
-    before(:example) do 
-      @car = Car.new
-    end
+    let(:car) { Car.new }
+
+    # 'let' is better than 'before' for
+    # setting up instance variables
+    # before(:example) do 
+    #   @car = Car.new
+    # end
 
     it "allows reading and writing for :make" do
-      @car.make = 'Test'
-      expect(@car.make).to eq('Test')
+      car.make = 'Test'
+      expect(car.make).to eq('Test')
     end
 
     it "allows reading and writing for :year" do
-      @car.year = 9999
-      expect(@car.year).to eq(9999)
+      car.year = 9999
+      expect(car.year).to eq(9999)
     end
 
     it "allows reading and writing for :colour" do
-      @car.colour = 'foo'
-      expect(@car.colour).to eq('foo')
+      car.colour = 'foo'
+      expect(car.colour).to eq('foo')
     end
 
     it "allows reading for :wheels" do
-      expect(@car.wheels).to eq(4)
+      expect(car.wheels).to eq(4)
     end
 
     it "allows writing for :doors" 
@@ -33,23 +37,26 @@ describe 'Car' do
 
   describe '.colours' do
 
+    let(:colours) { ['blue', 'black', 'red', 'green'] }
+
     it "returns an array of colour names" do
-      c = ['blue', 'black', 'red', 'green']
-      expect(Car.colours).to match_array(c)
+      expect(Car.colours).to match_array(colours)
     end
 
   end
 
   describe '#full_name' do
+
+    let(:honda) { Car.new(make: 'Honda', year: 2004, colour: 'blue') }
+    let(:new_car) { new_car = Car.new }
+
     it "returns a string in the expected format" do
-      @honda = Car.new(make: 'Honda', year: 2004, colour: 'blue')
-      expect(@honda.full_name).to eq('2004 Honda (blue)')
+      expect(honda.full_name).to eq('2004 Honda (blue)')
     end
 
     context 'when initialized with no arguments' do
       it 'returns a string using default values' do
-        car = Car.new
-        expect(car.full_name).to eq ('2007 Volvo (unknown)')
+        expect(new_car.full_name).to eq ('2007 Volvo (unknown)')
       end
     end
   end
