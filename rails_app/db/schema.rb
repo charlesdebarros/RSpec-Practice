392943854a29a7cd8117cd27031a54e8e51b6cc7
Charles De Barros <charlesdebarros@outlook.com>
2020-04-05 15:45:11 +0100

    Updating Rails and related gems to version 5.2.4.2

--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150918184537) do
+ActiveRecord::Schema.define(version: 2015_09_18_184537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "customers", force: :cascade do |t|
+  create_table "customers", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
