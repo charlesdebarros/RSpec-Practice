49000f3ff29a2cabe00d76dd43b61b6fdb21621e
Charles De Barros <charlesdebarros@outlook.com>
2021-11-20 12:21:11 +0000

    Implementing handling failure test * Returning a 422 code when failing to record an expense * Returning an error messagem when failing to record an expense

--- expenses_project/spec/unit/app/api_spec.rb
+++ expenses_project/spec/unit/app/api_spec.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 require './app/api'
+require './app/ledger'
 require 'rack/test'
 
 module ExpenseTracker
-  RecordResult = Struct.new(:success?, :expense_id, :error_message)
-
   RSpec.describe API do
     include Rack::Test::Methods
 
@@ -39,8 +38,26 @@ module ExpenseTracker
       end
 
       context 'when the expense fails validation' do
-        it 'returns an error message'
-        it 'respondes with a 422 (Unprocessable entity)'
+        let(:expense) { { 'some' => 'data' } }
+
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new( false, 417, 'Expense incomplete' ))
+        end
+
+        it 'returns an error message' do
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('error' => 'Expense incomplete')  
+        end
+
+        it 'respondes with a 422 (Unprocessable entity)' do
+          post '/expenses', JSON.generate(expense)
+
+          expect(last_response.status).to eq(422)
+        end
       end
     end
   end
